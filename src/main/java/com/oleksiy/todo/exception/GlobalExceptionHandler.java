4a01a89b917bf951603e7ad7640a09eabecbde21
Oleksiy <vinokurovlosa@gmail.com>
2023-05-24 23:42:54 +0300

    Rebuild GlobalExceptionHandler to rest. It needs upgrade

--- src/main/java/com/oleksiy/todo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/oleksiy/todo/exception/GlobalExceptionHandler.java
@@ -1,51 +1,39 @@
 package com.oleksiy.todo.exception;
 
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;import org.springframework.http.HttpStatus;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.web.bind.MissingPathVariableException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    @ExceptionHandler(NullEntityReferenceException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ModelAndView nullEntityReferenceExceptionHandler(HttpServletRequest request, NullEntityReferenceException exception) {
-        return getModelAndView(request, HttpStatus.BAD_REQUEST, exception);
-    }
-
-    @ExceptionHandler(EntityNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ModelAndView entityNotFoundExceptionHandler(HttpServletRequest request, EntityNotFoundException exception) {
-        return getModelAndView(request, HttpStatus.NOT_FOUND, exception);
-    }
-
-    @ExceptionHandler(AccessDeniedException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ModelAndView accessDeniedErrorHandler(HttpServletRequest request, Exception exception) {
-        return getModelAndView(request, HttpStatus.FORBIDDEN, exception);
-    }
-
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ModelAndView internalServerErrorHandler(HttpServletRequest request, Exception exception) {
-        return getModelAndView(request, HttpStatus.INTERNAL_SERVER_ERROR, exception);
+    public ResponseEntity<?> internalServerErrorHandler(Exception ex) {
+        logger.error("Exception raised = {} :: Class = {}", ex.getMessage(), "Exception");
+        return ResponseEntity.badRequest().body(ex.getMessage());
     }
 
-    private ModelAndView getModelAndView(HttpServletRequest request, HttpStatus httpStatus, Exception exception) {
-        logger.error("Exception raised = {} :: URL = {}", exception.getMessage(), request.getRequestURL());
-        ModelAndView modelAndView = new ModelAndView("error");
-        modelAndView.addObject("code", httpStatus.value() + " / " + httpStatus.getReasonPhrase());
-        modelAndView.addObject("message", exception.getMessage());
-        return modelAndView;
+    @ExceptionHandler
+    public ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        logger.error("Exception raised = {} :: Class = {}", ex.getMessage(), ex.getClass());
+        return ResponseEntity.badRequest().body(ex.getBindingResult().toString());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleNullEntityReferenceException(NullEntityReferenceException ex) {
+        logger.error("Exception raised = {} :: Class = {}", ex.getMessage(), ex.getClass());
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException ex) {
+        logger.error("Exception raised = {} :: Method = {}", ex.getMessage(), ex.getClass());
+        return ResponseEntity.notFound().build();
     }
 }
