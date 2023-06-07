44e73a9e89e329fc29246403f6714250e88cc855
Oleksiy <vinokurovlosa@gmail.com>
2023-06-07 23:17:28 +0300

    Update LoginController

--- src/main/java/com/oleksiy/todo/controller/LoginController.java
+++ src/main/java/com/oleksiy/todo/controller/LoginController.java
@@ -1,21 +1,58 @@
 package com.oleksiy.todo.controller;
 
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.oleksiy.todo.config.jwt.JwtTokenProvider;
+import com.oleksiy.todo.dto.AuthRequestDto;
+import com.oleksiy.todo.model.User;
+import com.oleksiy.todo.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+@AllArgsConstructor
 public class LoginController {
 
-    @GetMapping("/form-login")
-    public String loginForm() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
-            return "form-login";
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final UserService userService;
+
+    @PostMapping("login")
+    public ResponseEntity<Map<Object, Object>> login(@RequestBody AuthRequestDto requestDto) {
+        try {
+            String username = requestDto.getUsername();
+            authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(username, requestDto.getPassword()));
+            User user = userService.findByEmail(username);
+
+            if (user == null) {
+                throw new UsernameNotFoundException("User with username: " + username + " not found");
+            }
+
+            String token = jwtTokenProvider.createToken(username, user.getRole());
+
+            Map<Object, Object> response = new HashMap<>();
+            response.put("username", username);
+            response.put("token", token);
+
+            return ResponseEntity.ok(response);
+        } catch (AuthenticationException e) {
+            throw new BadCredentialsException("Invalid username or password");
         }
-        return "redirect:/users/all";
     }
 
 }
