1db765c8e242096fdd80ce5b5258560fbc754611
Oleksiy <vinokurovlosa@gmail.com>
2023-06-07 23:16:54 +0300

    Update SecurityConfig & Add jwt configurer in there & Add lines in yml

--- src/main/java/com/oleksiy/todo/config/SecurityConfig.java
+++ src/main/java/com/oleksiy/todo/config/SecurityConfig.java
@@ -1,17 +1,24 @@
 package com.oleksiy.todo.config;
 
+import com.oleksiy.todo.config.jwt.JwtConfigurer;
+import com.oleksiy.todo.config.jwt.JwtTokenProvider;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @Slf4j
 @EnableWebSecurity
@@ -19,20 +26,59 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class SecurityConfig {
 
+    UserDetailsService userServiceImpl;
+
+    JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    public SecurityConfig(UserDetailsService userDetailsService, JwtTokenProvider jwtTokenProvider) {
+        this.userServiceImpl = userDetailsService;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().permitAll()
+                .exceptionHandling(eh -> eh
+                        .authenticationEntryPoint(restAuthenticationEntryPoint())
                 )
-                .httpBasic(withDefaults())
-                .csrf().disable();
+                .httpBasic(hb -> hb
+                        .authenticationEntryPoint(restAuthenticationEntryPoint()) // Handles auth error
+                )
+                .csrf().disable()
+                .headers(h -> h
+                        .frameOptions() // for Postman, the H2 console
+                        .disable()
+                )
+                .sessionManagement(sm -> sm
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // no session
+                )
+                .authorizeHttpRequests(a -> a
+                        .requestMatchers("/login", "/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .apply(new JwtConfigurer(jwtTokenProvider));
         return http.build();
+    }
+
+    @Bean
+    AuthenticationEntryPoint restAuthenticationEntryPoint() {
+        return (request, response, authException) -> {
+            log.warn("Authentication for '{} {}' failed with error: {}",
+                    request.getMethod(), request.getRequestURL(), authException.getMessage());
+            response.sendError(UNAUTHORIZED.value(), authException.getMessage());
+        };
     }
 
 }
